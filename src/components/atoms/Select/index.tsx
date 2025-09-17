46aa24aa80c569f025989862131587e190329c3e
Thinley <thinley@Thinleys-MacBook-Air.local>
2025-09-17 22:23:57 +0100

    color fix

--- src/components/atoms/Select/index.tsx
+++ src/components/atoms/Select/index.tsx
@@ -14,6 +14,6 @@ interface Props {
 
 export default function Dropdown({options, onChange,instanceId }:Props) {
   return (
-    <Select instanceId={instanceId} options={options} onChange={onChange} className='text-primary' />
+    <Select instanceId={instanceId} options={options} onChange={onChange} className='text-dark' />
   );
 }
